4dea92e1dfd44be4ce3a7878de4a0ea711ac8faf
Yao-Xinchen <yao29@illinois.edu>
2025-01-17 17:24:46 -0600

    add debian.sh

---
+++ scripts/debian.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# cannot be run as root
+if [[ $EUID -eq 0 ]]; then
+    echo "Do not run as root. Exiting."
+    exit 1
+fi
+
+echo "Installing RISCV-tools..."
+sudo apt install autoconf automake autotools-dev curl python3 \
+python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential \
+bison flex texinfo gperf libtool patchutils bc zlib1g-dev \
+libexpat-dev ninja-build git cmake libglib2.0-dev libslirp-dev \
+libpixman-1-dev libgtk-3-dev
+mkdir -p /opt/riscv
+sudo chmod -R a+rwx /opt/riscv
+git clone --branch 2024.04.12 https://github.com/riscv/riscv-gnu-toolchain $HOME/riscv-gnu-toolchain_temp
+cd $HOME/riscv-gnu-toolchain_temp
+./configure --prefix=/opt/toolchains/riscv/ --enable-multilib
+make
+
+echo "Installing QEMU..."
+git clone https://git.qemu.org/git/qemu.git $HOME/qemu_temp --branch=v9.0.2
+patch -d $HOME/qemu_temp -p1 <../resources/qemu.patch
+cd $HOME/qemu_temp
+./configure --target-list=riscv64-softmmu --enable-sdl --enable-gtk --enable-vnc --enable-cocoa --enable-system --disable-werror
+make
+sudo make install
+
+echo "Cleaning up..."
+rm -rf $HOME/riscv-gnu-toolchain_temp
+rm -rf $HOME/qemu_temp
+
+echo "Do you want to add these to your PATH? (y/n)"
